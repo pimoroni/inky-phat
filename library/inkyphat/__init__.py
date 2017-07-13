a0d61832be9415fc4a065447d6bb879275505f3e
Phil Howard <phil@gadgetoid.com>
2017-07-13 16:42:18 +0100

    Fixed version string

--- library/inkyphat/__init__.py
+++ library/inkyphat/__init__.py
@@ -7,7 +7,7 @@ except ImportError:
     exit("This script requires the pillow module\nInstall with: sudo pip install pillow")
 
 
-__version__ = "0.0.1"
+__version__ = '0.0.2'
 
 WHITE = 0
 BLACK = 1
