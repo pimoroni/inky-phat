bf1d0a9303621f2808fb12620fec5d036ab8d187
Phil Howard <phil@gadgetoid.com>
2017-12-07 13:55:26 +0000

    Prep for v0.1.0

--- library/setup.py
+++ library/setup.py
@@ -39,7 +39,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
 
 setup(
     name            = 'inkyphat',
-    version         = '0.0.3',
+    version         = '0.1.0',
     author          = 'Philip Howard',
     author_email    = 'phil@pimoroni.com',
     description     = 'Inky pHAT Driver',
